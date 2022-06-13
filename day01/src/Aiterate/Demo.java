80e251c10b317445c85d866959cbd5cf35920ab0
Pony <2902673678@qq.com>
2022-06-13 19:31:42 +0800

    Merge branch 'dev'
    # Conflicts:
    #	day01/src/Aiterate/Demo.java

--- day01/src/Aiterate/Demo.java
+++ day01/src/Aiterate/Demo.java
@@ -2,11 +2,6 @@ package Aiterate;
 
 public class Demo {
     public static void main(String[] args) {
-        System.out.println("1111111111Master ");
-        System.out.println("1111111111Dev ");
-        System.out.println("1111111111Dev ");
-        System.out.println("1111111111Dev ");
-        System.out.println("1111111111Dev ");
-        System.out.println("1111111111Dev ");
+        System.out.println("1111111111");
     }
 }
