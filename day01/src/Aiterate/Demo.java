8bd8147fb0abfa248140d0a242edb90103943f64
Pony <2902673678@qq.com>
2022-06-13 19:52:32 +0800

    增加11111Dev

--- day01/src/Aiterate/Demo.java
+++ day01/src/Aiterate/Demo.java
@@ -4,5 +4,10 @@ public class Demo {
     public static void main(String[] args) {
         System.out.println("1111111111Master");
         System.out.println("1111111111Dev");
+        System.out.println("1111111111Dev");
+        System.out.println("1111111111Dev");
+        System.out.println("1111111111Dev");
+        System.out.println("1111111111Dev");
+        System.out.println("1111111111Dev");
     }
 }
